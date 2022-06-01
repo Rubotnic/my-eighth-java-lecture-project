330e3712c07450900cba5e47935a780a85444845
Rubotnic <info@rubotnic.com>
2022-06-01 20:04:50 +0200

    My eighth JAVA lecture project.

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,26 @@
+package se.lexicon;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        int numToGuess = ran.nextInt(500);
+        Scanner input = new Scanner(System.in);
+        boolean win = true;
+        while (win == true) {
+            System.out.println("Guess a number between 1 and 500");
+            int guess = input.nextInt();
+
+            if (numToGuess == guess) {
+                System.out.println("CONGRATS! This is the right number.");
+                win = false;
+            } else if (numToGuess > guess) {
+                System.out.println("Your number is too low");
+            } else if (numToGuess < guess) {
+                System.out.println("Your number is to high");
+            }
+
+        }
+    }
+}
